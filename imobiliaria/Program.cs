466766a75ac78014d6059e4837118557ec1ecee8
Mariaeduardaz <vitorvogel12@gmail.com>
2022-02-04 23:46:45 -0300

    Add project files.

---
+++ imobiliaria/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace imobiliaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("terrenos retangulares!");
+
+            Console.WriteLine("Qual a altura do terreno? ");
+
+            float altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a largura do terreno? ");
+
+            float largura = float.Parse(Console.ReadLine());
+
+            float area = altura * largura;
+            Console.WriteLine($" A area do seu terreno é de {area} ");
+
+            Console.ReadLine();
+            
+              
+
+
+        }
+    }
+}
